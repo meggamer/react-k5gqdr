99231a1b074eca654016aa78ef600a5f9bd696fc
meggamer <98605893+meggamer@users.noreply.github.com>
2023-05-05 12:49:37 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 function Page() {return React.createElement('div', null, [
   React.createElement('h1', null, `Hi ${Date.toLocaleString()}`),
   React.createElement('p', null, 'Hello'),
-  React.createElement('p', null, 'Hey'),
+  React.createElement('p', null, 'Hey')
 ]);
 }
   
