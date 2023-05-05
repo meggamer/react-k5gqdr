37c6bd747081c6d82e80eca0b37b55968da25b06
meggamer <98605893+meggamer@users.noreply.github.com>
2023-05-05 14:57:32 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style.css';
 
-function Page() {
-  return React.createElement('div', null, [
-    React.createElement('h1', null, `Hi ${Date().toLocaleString()}`),
-    React.createElement('p', null, 'Hello'),
-    React.createElement('p', null, 'Hey'),
-  ]);
+
+
+
+function Page() {return React.createElement('div', null, [
+  React.createElement('h1', null, `Hi ${Date().toLocaleString()}`),
+  React.createElement('p', null, 'Hello'),
+  React.createElement('p', null, 'Hey')
+]);
 }
+  
 
 const rootEL = document.querySelector('#root');
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(rootEl);
+/*const root = ReactDOM.createRoot(document.getElementById('root'));*/
 
-setInterval(function () {
+
+
+setInterval(function(){
   root.render(Page());
 }, 1000);
